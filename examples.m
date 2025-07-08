5a80cf0a72390faeb4c830ab4eead44b5076dd2f
buchacm <64734443+buchacm@users.noreply.github.com>
2025-07-08 12:08:35 +0200

    Add files via upload

---
+++ examples.m
@@ -0,0 +1,28 @@
+(* ::Package:: *)
+
+(* ::Input:: *)
+(*SetDirectory["/Users/manfredbuchacher/Documents/Uni-RICAM/nearSeparation"]*)
+
+
+(* ::Input:: *)
+(*<<nearSeparate.m*)
+
+
+(* ::Input:: *)
+(*nearSeparate[x y-x^2-y^2-x^2y^2,{x,y}]*)
+
+
+(* ::Input:: *)
+(*nearSeparate[x y-x^2y-x y^2-1,{x,y}]*)
+
+
+(* ::Input:: *)
+(*nearSeparate[x y-x^2 y-x^2y^2-x y^2-x-y-1,{x,y}]*)
+
+
+(* ::Input:: *)
+(*nearSeparate[x y-x^2y-x^2y^2-y-1,{x,y}]*)
+
+
+(* ::Input:: *)
+(**)
